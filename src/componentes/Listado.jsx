d73eec883b816484407156d32135b7e0501f33c8
Mauricio Cruz <mauriciogonzalolopezcruz@gmail.com>
2023-11-08 18:32:03 -0300

    se le agrego la funcionalidad de poder listar a lss usuarios

--- src/componentes/Listado.jsx
+++ src/componentes/Listado.jsx
@@ -6,12 +6,18 @@ function Listado(){
   const [listadoTarea,setListadoTarea] = useState(false);
   const [encabezadoListadoTarea,setEncabezadoListadoTarea] = useState(false);
 
+  const {data: usuarioData} = useFetch("#"); 
+  const [listadoUsuario,setListadoUsuario] = useState(false);
+  const [encabezadoListadoUsuario,setEncabezadoListadoUsuario] = useState(false);
+
    const inicializar = () =>{
     //filas
     setListadoTarea(false);
+    setListadoUsuario(false);
 
     //encabezados
     setEncabezadoListadoTarea(false);
+    setEncabezadoListadoUsuario(false);
   }
   
   //tarea
@@ -20,27 +26,53 @@ function Listado(){
     setListadoTarea(true);
     setEncabezadoListadoTarea(true);
   }
+  //usuario
+  const listarUsuario = () =>{
+    inicializar();
+    setListadoUsuario(true);
+    setEncabezadoListadoUsuario(true);
+  }
 
   return (
     <div className="navListar">
       <button onClick={listarTarea}>Listar tarea</button>
+      <button onClick={listarUsuario}>Listar usuario</button>
 
-      {
-       encabezadoListadoTarea && <div>
+      {//tarea
+       encabezadoListadoTarea && <div className='encabezadoProducto'>
           <div>Titulo</div>
           <div>IdAutor</div>
           <div>IdUsuario</div>
           <div>cuerpo</div>
           <div>categorias</div>
         </div>
-      }    
-      {
+      }
+      
+      {//usuario
+         encabezadoListadoUsuario && <div className='encabezadoProducto'>
+          <div>Titulo</div>
+          <div>IdAutor</div>
+          <div>IdUsuario</div>
+          <div>cuerpo</div>
+          <div>categorias</div>
+        </div>
+      }
+      
+      {//tarea
         listadoTarea && tareaData?.map((tarea) => (
-        <div key={tarea.id} className='listdo'>
+        <div key={tarea.id}>
           <div>{tarea.nombre}</div>
           <div>{tarea.tipo}</div>
           <div>{tarea.precio}</div>
           <div>{tarea.gramos}</div>
+        </div>
+        ))
+      }
+      {//usuario
+        listadoUsuario && tareaData?.map((usuario) => (
+        <div key={usuario.id}>
+          <div>{usuario.nombre}</div>
+          <div>{usuario.email}</div>
         </div>
         ))
       }
