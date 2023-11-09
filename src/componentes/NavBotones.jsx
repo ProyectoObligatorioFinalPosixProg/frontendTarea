d87231bcf29c1c930495f71dd014df8f716585cc
Mauricio Cruz <mauriciogonzalolopezcruz@gmail.com>
2023-11-09 16:06:04 -0300

    se agrego la funcionalidades dentro del componente NavBotones para manejar el nav

--- src/componentes/NavBotones.jsx
+++ src/componentes/NavBotones.jsx
@@ -1,12 +1,26 @@
-import React, {useState} from "react";
+import React from "react";
 
-function NavBotones(){
+function NavBotones(prop){
+    const inicializar=()=>{
+        prop.click(false,false,false); //tarea, usuario, listadoTarea
+    }
+    const abilitarFormularioTarea=()=>{
+        inicializar();
+        prop.click(true,false,false); //tarea, usuario, listadoTarea
+    }
+    const abilitarFormularioRegistrarse=()=>{
+        inicializar();
+        prop.click(false,true,false); //tarea, usuario, listadoTarea
+    }
+    const abilitarListadoTarea=()=>{
+        inicializar();
+        prop.click(false,false,true); //tarea, usuario, listadoTarea
+    }
     return(
         <>
-        <button>Crear Tarea</button>
-        <button>Registrarse</button>
-        <button>Listar Tareas</button>
-        <button>Listar Usuarios</button>
+        <button onClick={abilitarFormularioTarea}>Crear Tarea</button>
+        <button onClick={abilitarFormularioRegistrarse}>Registrarse</button>
+        <button onClick={abilitarListadoTarea}>Listar Tareas</button>
         </>
     );
 }
